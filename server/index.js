ecc86c33baaf05e8606e9bbbfe3a84da8b6460b7
thaismr <thaismr@users.noreply.github.com>
2019-07-08 19:54:06 +0100

    mongoose

--- server/index.js
+++ server/index.js
@@ -1,20 +1,28 @@
 const { ApolloServer } = require('apollo-server')
+const { RedisCache } = require('apollo-server-cache-redis')
+const mongoose = require('mongoose')
+//const MongoStore = require('connect-mongo')(session)
 const { typeDefs, resolvers } = require('./schema')
 
-const pages = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
+// Replace your passwords, server addresses, etc:
+const private = require('../private/private')
+
+const { MONGO_URI } = private
+if (!MONGO_URI) {
+  throw new Error('You must provide a MongoLab URI')
+}
+
+mongoose.Promise = global.Promise
+mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+const db = mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance'))
+  .on('error', () => console.log('Error connecting to MongoLab:'))
+
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  dataSources: db,
   context: ({ req }) => {
     const token = req.headers.authorization || ''
     const user = getUser(token)
--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -5,6 +5,13 @@ const resolvers = {
     getAuthors: () => authors,
     getPublications: () => Publication
   },
+  Mutation: {
+    async addAuthor(parent, args, ctx, info) {
+      const emailTaken = await author.email
+      if (emailTaken) throw new Error('Email already taken.')
+      return author
+    }
+  },
   Publication: {
     __resolveType(obj, ctx, info) {
       if (obj.title) return 'Page'
